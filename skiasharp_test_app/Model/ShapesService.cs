ce1241c1937ffc8b6c9e541610063de0c070b2e1
Валентин Борнашов <shipverty@vk.com>
2024-09-15 23:14:30 +0700

    feature(ShapesService): добавлен сервис для генерации элементов

---
+++ skiasharp_test_app/Model/ShapesService.cs
@@ -0,0 +1,19 @@
+﻿using SkiaSharp;
+
+namespace skiasharp_test_app.Model;
+
+public static class ShapesService
+{
+    public static SKPath GetResistor()
+    {
+        var path = new SKPath();
+        path.MoveTo(0, 10);
+        path.LineTo(20, 10);
+        path.AddRect(new SKRect(20, 0, 80, 20));
+        path.MoveTo(80, 10);
+        path.LineTo(100, 10);
+        path.Close();
+        
+        return path;
+    }
+}
