47a4f9620a36cba67dc07429aafba92214eb3bf8
Валентин Борнашов <shipverty@vk.com>
2024-09-15 23:28:18 +0700

    feature(Shape, Resistor): добавлен абстрактный класс фигуры и класс резистора

---
+++ skiasharp_test_app/Model/Shapes/Resistor.cs
@@ -0,0 +1,22 @@
+﻿using SkiaSharp;
+
+namespace skiasharp_test_app.Model.Shapes;
+
+public class Resistor : Shape
+{
+    public override int X { get; set; }
+    
+    public override int Y { get; set; }
+    
+    public override SKPaint Paint { get; set; }
+    
+    public override bool ContainsPoint()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override SKPath GetPath()
+    {
+        throw new NotImplementedException();
+    }
+}
